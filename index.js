028ce7c0cf2272802fdd930498b20c33b938036a
Jane Saldana <janesaldana@Janes-Mac-mini.local>
2023-04-14 17:31:25 -0500

    block 13 Greater than 5

---
+++ index.js
@@ -0,0 +1,19 @@
+//question one: Is Truthy
+
+//question two: Number Line
+
+//question three: Greater than 5
+//take two values
+let num1 = 6;
+let num2 = 4
+// if both values are greater than or equal to 5
+if (num1 >= 5 && num2 >=5) {
+    //log out true
+    console.log(true)
+    } else {
+        //else false
+        console.log(false)
+    }
+
+
+//question four: Pair and Compare
